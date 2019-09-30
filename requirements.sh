d22010960bc7c76ae9b4776a8951e63f0f1c1f4b
Vanessa Böhm <22053912+VMBoehm@users.noreply.github.com>
2019-09-30 08:42:19 -0700

    updated abseil-py to requirements

--- requirements.sh
+++ requirements.sh
@@ -3,3 +3,4 @@ pip install -c bccp nbodykit
 pip install https://github.com/rainwoodman/vmad/archive/master.zip
 pip install https://github.com/bccp/abopt/archive/master.zip
 pip install https://github.com/rainwoodman/fastpm-python/archive/master.zip
+pip install https://github.com/abseil/abseil-py
