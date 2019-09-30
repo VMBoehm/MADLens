ea3eaf215624e526e7cde71800f55f8de552133d
Francois <fr.eiffel@gmail.com>
2019-09-30 12:38:26 -0700

    Working setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,6 @@ setup(name='MADLens',
       author_email='vboehm@berkeley.edu',
       license='GNU GPLv3',
       packages=['MADLens'],
-      install_requires=['numpy','cython','mpi4py','nbodykit',
-                        'vmad',
-                        'abopt','absl-py','fastpm'],
+      install_requires=['nbodykit', 'dask[array]', 'vmad', 'abopt', 'absl-py','fastpm'],
       #dependency_links=['https://github.com/rainwoodman/fastpm-python','http://github.com/bccp/nbodykit']
       )
