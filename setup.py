15f3333c38e90074107096c02d655f32ed1aac9d
Boehm, Vanessa Martina (vboehm) <boehmvanessa@gmail.com>
2019-09-30 12:11:40 -0700

    setup.py install runs into errors when installing dependencies nbodykit and fastpm

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 setup(name='MADLens',
-      version='0.1',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
       description='a differentiable lensing simulator',
       url='http://github.com/VMBoehm/MADLens',
-      author='Vanessa Martina Böhm',
+      author='Vanessa Martina Boehm',
       author_email='vboehm@berkeley.edu',
       license='GNU GPLv3',
       packages=['MADLens'],
-      dependency_links=['http://github.com/bccp/nbodykit', 'https://github.com/rainwoodman/vmad', 'https://github.com/bccp/abopt', 'https://github.com/rainwoodman/fastpm-python']
+      install_requires=['numpy','cython','mpi4py','nbodykit',
+                        'vmad',
+                        'abopt','absl-py','fastpm'],
+      #dependency_links=['https://github.com/rainwoodman/fastpm-python','http://github.com/bccp/nbodykit']
       )
