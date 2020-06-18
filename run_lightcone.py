c581e5ea8cb7e1b32a43661e4b69b9ea114599f1
Vanessa Boehm <boehmvanessa@gmail.com>
2020-06-18 14:40:28 -0700

    forward model mid res running (with interp and PGD)

--- run_lightcone.py
+++ run_lightcone.py
@@ -27,14 +27,14 @@ flags.DEFINE_integer('N_steps',40,'number of fastPM steps')
 #bounds from KIDS contours, default values from Planck2015
 flags.DEFINE_float('Omega_m',0.3089,'total matter density', lower_bound=0.1, upper_bound=0.5)
 flags.DEFINE_float('sigma_8',0.8158,'amplitude of matter fluctuations', lower_bound=0.4, upper_bound=1.3)
-flags.DEFINE_boolean('PGD',False,'whether to use PGD sharpening')
+flags.DEFINE_boolean('PGD',True,'whether to use PGD sharpening')
 flags.DEFINE_integer('B',2,'force resolution factor')
-flags.DEFINE_spaceseplist('zs_source',['0.5','1.0','1.5'],'source redshifts')
+flags.DEFINE_spaceseplist('zs_source',['1.0'],'source redshifts')
 flags.DEFINE_boolean('interpolate',True,'whether to interpolate between snapshots')
 flags.DEFINE_boolean('debug',True,'debug mode allows to run repeatedly with the same settings')
 flags.DEFINE_boolean('save3D',False,'whether to dump the snapshots, requires interp to be set to False')
 flags.DEFINE_boolean('save3Dpower', False, 'whether to measure and save the power spectra of the snapshots')
-flags.DEFINE_enum('mode', 'forward', ['forward','backprop'],'whether to run the forward model only or include backpropagation')
+flags.DEFINE_enum('mode', 'backprop', ['forward','backprop'],'whether to run the forward model only or include backpropagation')
 flags.DEFINE_boolean('analyze', False, 'whether to print out resource usage')
 flags.DEFINE_string('label', 'med_res', 'label of this run')
 old_print = print
@@ -123,28 +123,12 @@ def main(argv):
             save_2Dmap(kmap,mapfile)
             print('2D map #%d at z_s=%.1f dumped to %s'%(ii,z_source,mapfile))
             
-            bink,binpow,N = get_2Dpower(kmap)
-            len_k = len(bink)
-            if rank ==0:
-                if ii==0 and jj==0:
-                    binpows        = np.zeros((len(params['zs_source']),params['N_maps'],len_k))
-                    binpows[jj,ii] = binpow
 
     end = time.time()
 
     print('time taken per sim in min %d'%((end-sims_start)/(params['N_maps']*len(params['zs_source']))))
     print('time takes before sims in min %d'%(sims_start-start))
 
-    """ ------------------------- save average cls ----------------------------------"""
-    if rank==0:
-        for jj,z_source in enumerate(params['zs_source']):
-            binpow       = np.mean(binpows[jj], axis=0)
-            binpow_std   = np.std(binpows[jj], axis=0)
-
-            clfile       = os.path.join(dirs['cls'],'mean_cl_zsource%d_averaged_over_%dmaps.npy'%(z_source*10,params['N_maps']))
-
-            np.save(clfile,[bink,binpow,binpow_std,N])
-            print('cls dumped to %s'%clfile)
 
 if __name__ == '__main__':
   app.run(main)
