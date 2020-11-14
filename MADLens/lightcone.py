72e0ac19deb7696c76ac877ec4e1cd5dabb72aa1
maxelee <maxlee1993@berkeley.edu>
2020-11-14 10:11:48 -0800

    just adding fd for one kick function--its working

--- MADLens/lightcone.py
+++ MADLens/lightcone.py
@@ -1,11 +1,12 @@
 from vmad import autooperator, operator
 from vmad.core import stdlib
+from vmad.core.stdlib.finite_operator import finite_operator
 from vmad.core.symbol import Literal, ListPlaceholder
 from vmad.lib import fastpm
 from vmad.lib import linalg
 from vmad.lib.fastpm import FastPMSimulation, ParticleMesh
 import numpy
-from vmad.lib.linalg import sum, mul
+from vmad.lib.linalg import sum, mul, broadcast_to
 import scipy 
 from mpi4py import MPI
 import numpy as np
@@ -526,19 +527,22 @@ class WLSimulation(FastPMSimulation):
 
     @autooperator('rho, Om0->kmaps')
     def run(self, rho, Om0):
-        
+            
         rhok = fastpm.r2c(rho)
+
+        # Calculate EH power for initial modes
         norm = normalize(8, self.cosmo.Omega0_m, 'EH')
         norm = (self.cosmo.sigma8/norm)**2
         transfer =  get_Pk_EH(Om0, cosmo=self.cosmo, z=0, k=self.ks)**.5*norm**.5/self.pm.BoxSize.prod()**.5
         digitizer = fastpm.apply_digitized.isotropic_wavenumber(self.ks)
         rhok= fastpm.apply_digitized(x=rhok, tf=transfer, digitizer=digitizer, kind='wavenumber', mode='amplitude')
-        #stdlib.watchpoint(rhok, lambda rhok: print(rhok))
+
+
         dx, p  = self.firststep(rhok)
         pt     = self.pt
         stages = self.stages
         q      = self.q
-
+        FactoryCache = fastpm.CosmologyFactory()
         zero_map = Literal(self.mappm.create('real', value=0.))
         kmaps  = [zero_map for ds in self.ds]
         
@@ -551,7 +555,10 @@ class WLSimulation(FastPMSimulation):
             ac = (ai * af) ** 0.5
 
             # kick (update momentum)
-            dp = f * (self.KickFactor(ai, ai, ac) * 1.5 * Om0)
+            kick = finite_operator(Om0, lambda Om0, support=self.support, FactoryCache=FactoryCache, ai=ai, af=af, ac=ac:
+                                   fastpm.KickFactor(Om0, support, FactoryCache, ai, ai, ac), epsilon=1e-2, mode='central')
+            kick = broadcast_to(kick * 1.5 * Om0,stdlib.eval(f, lambda f: f.shape))
+            dp = f * kick
             p  = p + dp
 
             # drift (update positions)
